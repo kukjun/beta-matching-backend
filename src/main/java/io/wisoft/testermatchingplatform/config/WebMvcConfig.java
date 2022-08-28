ab715374633e2be41636c9128830bf46049e4aae
Kukjun Lee <lxx3380@gmail.com>
2022-08-28 11:51:03 +0900

    <featrue> : <frontend Session 연결>
    Frontend와 Session 연결을 위한 작업을 진행함.
    * CORS 설정 시, Header 허용 설정들을 추가해야 한다.
    
    Resolves: #issueNo
    See also: #issueNo

--- src/main/java/io/wisoft/testermatchingplatform/config/WebMvcConfig.java
+++ src/main/java/io/wisoft/testermatchingplatform/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
