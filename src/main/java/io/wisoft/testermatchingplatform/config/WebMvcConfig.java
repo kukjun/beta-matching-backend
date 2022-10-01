6b1b7d0ff3f7c07ce1165d5aef1f6fee71451a52
Kukjun Lee <lxx3380@gmail.com>
2022-10-01 22:41:50 +0900

    <feature> : <기능 구현을 위해 Filter 제외 처리>
    jwt를 적용하는 코드 적용이 너무 빠르다고 생각해서 회의 후 이를 제외함.
    
    Resolves: #issueNo
    See also: #issueNo

--- src/main/java/io/wisoft/testermatchingplatform/config/WebMvcConfig.java
+++ src/main/java/io/wisoft/testermatchingplatform/config/WebMvcConfig.java
@@ -1,33 +1,26 @@
 package io.wisoft.testermatchingplatform.config;
 
-import io.wisoft.testermatchingplatform.jwt.JwtAuthResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @EnableWebMvc
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private  final JwtAuthResolver authAccountResolver;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3_600);
+
     }
 
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(authAccountResolver);
-    }
 }
